8d74b8b93afa794eca2dc37800875f3411eb6053
carnage999-max <jamesezekiel039@gmail.com>
2025-06-20 05:25:05 +0100

    Updated start.sh to use single worker and threads for gunicorn

--- start.sh
+++ start.sh
@@ -2,7 +2,7 @@
 # start.sh
 
 # Start Gunicorn in background
-gunicorn task_mgt.wsgi:application --bind 0.0.0.0:$PORT &
+gunicorn task_mgt.wsgi:application --bind 0.0.0.0:$PORT --workers 1 --threads 1 --timeout 60 &
 
 # Start Celery
-celery -A task_mgt worker --loglevel=info
+celery -A task_mgt worker --pool=solo --loglevel=info
